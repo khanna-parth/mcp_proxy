b783ec5fa4b426b112043d78c3ab4b3a85a1f3f6
parthk5 <khanna.parth@outlook.com>
2025-09-08 01:27:22 -0700

    bug fix

--- src/mcp_proxy/models/tool_override.py
+++ src/mcp_proxy/models/tool_override.py
@@ -1,5 +1,5 @@
 
-from typing import Awaitable, Callable, List, Dict, Any, Optional
+from typing import Awaitable, Callable, List, Dict, Any, Optional, Protocol
 from pydantic import BaseModel
 from mcp import types
 from mcp_proxy.clients.sse_client import MCPClient
@@ -8,11 +8,22 @@ class ToolResult(BaseModel):
     text: str
     error: bool
 
+class OverrideFn(Protocol):
+    def __call__(
+        self,
+        tool_name: str,
+        arguments: Dict[str, Any],
+        client: "MCPClient",
+        **kwargs: Any
+    ) -> Awaitable["ToolResult"]:
+        ...
+
 
 # override_fn -> (tool_name, tool_args, mcp_client, kwargs)
 class ToolOverride(BaseModel):
     base_tool: types.Tool
-    override_fn: Callable[[str, Dict[str, Any], MCPClient], Awaitable[ToolResult]]
+    override_fn: OverrideFn
+    # override_fn: Callable[[str, Dict[str, Any], MCPClient], Awaitable[ToolResult]]
     
     async def call_with_original_access(
         self, 
